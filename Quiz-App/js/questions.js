87d1f2daf7bed7a62fc2fe38cf7aceb651d29ef6
ashish614 <97785632+ashish614@users.noreply.github.com>
2024-01-14 17:13:32 +0530

    Add files via upload

---
+++ Quiz-App/js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which device is required for the internet connection?",
+    answer: "Modem",
+    options: [
+      "Modem",
+      "Router",
+      "LAN",
+      "Pen Drive"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+      "Null type",
+      "Undefined type",
+      "Number type",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext PreProgramming",
+      "HomeText Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following scoping type does JavaScript use?",
+    answer: "Lexical",
+    options: [
+      "Sequential",
+      "Segmental",
+      "Lexical",
+      "Literal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not an error in JavaScript?",
+    answer: "Division by zero",
+    options: [
+      "Missing of Bracket",
+      "Division by zero",
+      "Syntax error",
+      "Missing of semicolons"
+    ]
+  },
+];
